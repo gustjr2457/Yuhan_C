87023817480a96088fcb2de13269f2861afa183b
gustjr2457 <gustjr2457@naver.com>
2023-05-24 20:47:00 +0900

    gustjr2457 과제 수정(애니메이션)

--- homework01/Cproject.c
+++ homework01/Cproject.c
@@ -9,149 +9,140 @@ void hideCursor() {
 	CONSOLE_CURSOR_INFO cursorInfo;
 	cursorInfo.dwSize = 1;
 	cursorInfo.bVisible = FALSE;
-	SetConsoleCursorInfo(consoleHandle, &cursorInfo);	
+	SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 }
 
 
-void GotoXYZero(){
+void GotoXYZero() {
 	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO cursorInfo;
 	SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 	COORD pos = { 0, 0 };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-char title[15][40];
-void InsertString(const char* string, int y){
-	int length = strlen(string);
-	int middle = (40-(length+1))/2;
-	for (int a = 0; a <=(length-1); a++){
-		title[y][a + middle] = string[a];
-		GotoXYZero();
-		printf("%sinput>", title);
-	}
-	Sleep(16);
-	
-}
+char title[30][80];
 
 
-void screen_reset(){
-	for (int i=0; i<15; i++) {
-		for (int j=0; j<40; j++){
-			if (i == 0 || i == 14)
+void start_screen() {
+	hideCursor();
+	for (int i = 0; i < 30; i++) {
+		for (int j = 0; j < 80; j++) {
+			if (i == 0 || i == 29)
 				title[i][j] = '-';
-			else if (j == 0 || j == 38)
+			else if (j == 0 || j == 78)
+				title[i][j] = '|';
+			else
+				title[i][j] = '#';
+			if ((i == 0 && j == 0) || (i == 29 && j == 78) ||
+				(i == 0 & j == 78) || (i == 29 && j == 0))
+				title[i][j] = '@';
+		}
+		title[i][79] = '\n';
+	}
+	title[29][79] = '\0';
+	for (int i = 1; i < 29; i++) {
+		for (int j = 1; j < 78; j++) {
+			title[i][j] = ' ';
+			GotoXYZero();
+		}
+		printf("%s\n", title);
+		Sleep(16);
+	}
+}
+
+void screen_reset() {
+	hideCursor();
+	for (int i = 0; i < 30; i++) {
+		for (int j = 0; j < 80; j++) {
+			if (i == 0 || i == 29)
+				title[i][j] = '-';
+			else if (j == 0 || j == 78)
 				title[i][j] = '|';
 			else
 				title[i][j] = ' ';
-			if ((i==0 && j==0) || (i==14 && j==38) || 
-				(i==0 & j==38) || (i==14 && j==0))
+			if ((i == 0 && j == 0) || (i == 29 && j == 78) ||
+				(i == 0 & j == 78) || (i == 29 && j == 0))
 				title[i][j] = '@';
 		}
-		title[i][39] = '\n';
+		title[i][79] = '\n';
 	}
-	title[14][39] = '\0';
+	title[29][79] = '\0';
 }
 
-void game_screen(char* string) {
+void InsertString(const char* string, int y) {
+	int length = strlen(string);
+	int middle = (80 - (length + 1)) / 2;
+	for (int a = 0; a <= (length - 1); a++) {
+		title[y][a + middle] = string[a];
+		GotoXYZero();
+		printf("%s\ninput> ", title);
+	}
+	Sleep(16);
 
-	if (string[0] == 'M') {
-		system("cls");
-		for (int i=0; i<15; i++) {
-			for (int j=0; j<40; j++){
-				if (i == 0 || i == 14)
-					title[i][j] = '-';
-				else if (j == 0 || j == 38)
-					title[i][j] = '|';
-				else
-					title[i][j] = ' ';
-				if ((i==0 && j==0) || (i==14 && j==38) || 
-					(i==0 & j==38) || (i==14 && j==0))
-					title[i][j] = '@';
-			}
-			title[i][39] = '\n';
-		}
-		InsertString("Dancing without the professor knowing", 5);
-		InsertString("1.Start", 8);
-		InsertString("2.Help", 9);
-		InsertString("3.Exit", 10);
+}
+
+void game_screen(int choose) {
+	hideCursor();
+	if (choose == 1) {
+		screen_reset();
+		InsertString("Dancing without the professor knowing", 10);
+		InsertString("1.Start", 16);
+		InsertString("2.Help", 17);
+		InsertString("3.Exit", 18);
 	}
-	else if (string[0] == 'H') {
-		for (int i=0; i<15; i++) {
-			for (int j=0; j<40; j++){
-				if (i == 0 || i == 14)
-					title[i][j] = '-';
-				else if (j == 0 || j == 38)
-					title[i][j] = '|';
-				else
-					title[i][j] = ' ';
-				if ((i==0 && j==0) || (i==14 && j==38) || 
-					(i==0 & j==38) || (i==14 && j==0))
-					title[i][j] = '@';
-			}
-			title[i][39] = '\n';
-		}
-		InsertString("Press the space bar to dance", 5);
-		InsertString("when the professor is not watching!", 6);
-		InsertString("Return to Main Screen?(1.yes/2. no)", 9);
+	else if (choose == 2) {
+		screen_reset();
+		InsertString("Press the space bar to dance", 10);
+		InsertString("when the professor is not watching!", 11);
+		InsertString("Return to Main Screen?(1.yes/2. no)", 18);
 	}
-	else if (string[0] == 'E') { 
-		for (int i=0; i<15; i++) {
-			for (int j=0; j<40; j++){
-				if (i == 0 || i == 14)
-					title[i][j] = '-';
-				else if (j == 0 || j == 38)
-					title[i][j] = '|';
-				else
-					title[i][j] = ' ';
-				if ((i==0 && j==0) || (i==14 && j==38) || 
-					(i==0 & j==38) || (i==14 && j==0))
-					title[i][j] = '@';
-				}
-				title[i][39] = '\n';
-		}
-		InsertString("input number 1 to exit", 7);
+	else if (choose == 3) {
+		screen_reset();
+		InsertString("input number 1 to exit", 15);
 	}
-	title[14][39] = '\0';	
 }
 
 
-int main(){
+
+int main() {
 	int type1 = 1;
 	int type2 = 0;
-	char main[5] = "Main";
-	char help[5] = "Help";
-	char exit[5] = "Exit";
-	hideCursor();
-	while(type1){	
-		game_screen(main);
+	
+	system("mode con:cols=100 lines=40");
+	system("cls");
+	start_screen();
+	while (type1) {
+		game_screen(1);
 		scanf("%d", &type1);
-		if (type1 == 3){
+		if (type1 == 3) {
 			screen_reset();
-			game_screen(exit);
+			game_screen(3);
 			scanf("%d", &type2);
-			if(type2 == 1)
+			if (type2 == 1) {
+				system("cls");
 				type1 = 0;
+			}
 			else
-				system("cls");
-				continue;
+				GotoXYZero();
+			continue;
 		}
-		else if (type1 == 2){
+		else if (type1 == 2) {
 			type2 = 1;
-			while(type2){
-				system("cls");
-				game_screen(help);
+			while (type2) {
+				GotoXYZero();
+				game_screen(2);
 				scanf("%d", &type2);
-				if (type2 == 1){
-					system("cls");
+				if (type2 == 1) {
+					GotoXYZero();
 					type2 = 0;
 				}
 				else
-					type2 = 1;			
-			}			
+					type2 = 1;
+			}
 		}
-		else{
-			system("cls");
+		else {
+			GotoXYZero();
 			continue;
 		}
 	}
