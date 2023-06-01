cf1e7e4c09c93a5c6dfb316926c9c4c70742a6a7
gustjr2457 <gustjr2457@naver.com>
2023-06-02 02:07:01 +0900

    gustjr2457 게임 맵 틀 제작, 플레이어 틀 제작

--- homework01/Cproject.c
+++ homework01/Cproject.c
@@ -4,6 +4,13 @@
 #include <windows.h>
 #include <time.h>
 #include <conio.h>
+#include <stdbool.h>
+
+#define WIDTH 90
+#define HEIGHT 50
+
+char title[30][80];
+int MainGameScreen[HEIGHT][WIDTH];
 
 void hideCursor() {
 	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -13,7 +20,6 @@ void hideCursor() {
 	SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 }
 
-
 void GotoXYZero() {
 	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO cursorInfo;
@@ -22,8 +28,34 @@ void GotoXYZero() {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-char title[30][80];
+void BackGround(int PlayerX, int PlayerY) {
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			if (y == 0 || y == (HEIGHT - 1))
+				MainGameScreen[y][x] = 0;
+			else if (x == 0 || x == (WIDTH - 1))
+				MainGameScreen[y][x] = 0;
+			else if (x == PlayerX && y == PlayerY)
+				MainGameScreen[y][x] = 2;
+			else
+				MainGameScreen[y][x] = 1;
+		}
+	}
+}
 
+void BackGroundString() {
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			if (MainGameScreen[y][x] == 0)
+				printf("\x1b[47m \x1b[0m");
+			else if (MainGameScreen[y][x] == 1)
+				printf("\x1b[40m \x1b[0m");
+			else if (MainGameScreen[y][x] == 2)
+				printf("\x1b[42m \x1b[0m");
+		}
+		printf("\n");
+	}
+}
 
 void start_screen() {
 	hideCursor();
@@ -36,7 +68,7 @@ void start_screen() {
 			else
 				title[i][j] = '#';
 			if ((i == 0 && j == 0) || (i == 29 && j == 78) ||
-				(i == 0 & j == 78) || (i == 29 && j == 0))
+				(i == 0 && j == 78) || (i == 29 && j == 0))
 				title[i][j] = '@';
 		}
 		title[i][79] = '\n';
@@ -63,7 +95,7 @@ void screen_reset() {
 			else
 				title[i][j] = ' ';
 			if ((i == 0 && j == 0) || (i == 29 && j == 78) ||
-				(i == 0 & j == 78) || (i == 29 && j == 0))
+				(i == 0 && j == 78) || (i == 29 && j == 0))
 				title[i][j] = '@';
 		}
 		title[i][79] = '\n';
@@ -74,12 +106,12 @@ void screen_reset() {
 void InsertString(const char* string, int y) {
 	int length = strlen(string);
 	int middle = (80 - (length + 1)) / 2;
-	for (int a = 0; a <= (length - 1); a++) {
+	for (int a = 0; a < length; a++) {
 		title[y][a + middle] = string[a];
-		GotoXYZero();
-		printf("%s\ninput> ", title);
-		Sleep(16);
 	}
+	GotoXYZero();
+	printf("%s\n", title);
+	Sleep(16);
 }
 
 void game_screen(int choose) {
@@ -105,27 +137,49 @@ void game_screen(int choose) {
 
 
 
+
 int main() {
 	int type1 = 1;
 	int type2 = 0;
-	
-	system("mode con:cols=100 lines=40");
+
+	system("mode con cols=100 lines=40");
 	system("cls");
 	start_screen();
 	while (type1) {
 		game_screen(1);
 		type1 = getch() - '0';
-		if (type1 == 3) {
-			screen_reset();
-			game_screen(3);
-			type2 = getch() - '0';
-			if (type2 == 1) {
-				system("cls");
-				type1 = 0;
-			}
-			else
+		if (type1 == 1) {
+			int pX = 1;
+			int pY = 1;
+			int ch;
+			system("mode con cols=91 lines=51");
+			while (true) {
+				BackGround(pX, pY);
+				BackGroundString();
+				hideCursor();
 				GotoXYZero();
-			continue;
+
+				if (GetAsyncKeyState('W') & 0x8000) {
+					if (pY != 1)
+						pY--;
+				}
+				if (GetAsyncKeyState('A') & 0x8000) {
+					if (pX > 2)
+						pX = pX - 2;
+					if (pX <= 2)
+						pX = 1;
+				}
+				if (GetAsyncKeyState('S') & 0x8000) {
+					if (pY != (HEIGHT - 2))
+						pY++;
+				}
+				if (GetAsyncKeyState('D') & 0x8000) {
+					if (pX < (WIDTH - 3))
+						pX = pX + 2;
+					if (pX >= (WIDTH - 3))
+						pX = WIDTH - 2;
+				}
+			}
 		}
 		else if (type1 == 2) {
 			type2 = 1;
@@ -140,6 +194,18 @@ int main() {
 				else
 					type2 = 1;
 			}
+		}
+		else if (type1 == 3) {
+			screen_reset();
+			game_screen(3);
+			type2 = getch() - '0';
+			if (type2 == 1) {
+				system("cls");
+				type1 = 0;
+			}
+			else
+				GotoXYZero();
+			continue;
 		}
 		else {
 			GotoXYZero();
